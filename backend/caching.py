0616621355cb64a5a00a600653319009dfbfd5b9
QEDK <1994constant@gmail.com>
2021-03-07 16:23:17 +0530

    :rocket: set up last block call

--- backend/caching.py
+++ backend/caching.py
@@ -1,7 +1,22 @@
+import os
 import redis
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-r = redis.Redis(host=os.getenv('DB_HOST'), password=os.getenv('DB_PASSWORD'))
+r = redis.Redis(host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'), password=os.getenv('DB_PASSWORD'),
+	decode_responses=True)
+s = requests.Session()
 
-def get_last_block():
+def set_last_block():
+	next_block = last_known_block = int(r.get("last_included_block"))
+	while True:
+		if s.get(f"https://apis.matic.network/api/v1/matic/block-included/{next_block + 1}").json()["message"] != "success":
+			break
+		else:
+			next_block += 1
+	if next_block > last_known_block:
+		r.set("last_included_block", next_block)
+
+if __name__ == "__main__":
+	set_last_block()
