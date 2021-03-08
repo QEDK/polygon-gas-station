4f5693a607f4038105075a8fc3452075cd8f7deb
QEDK <1994constant@gmail.com>
2021-03-08 16:14:52 +0530

    :new: Add basic scheduler

--- backend/caching.py
+++ backend/caching.py
@@ -1,22 +1,27 @@
 import os
+import time
 import redis
 import requests
 from dotenv import load_dotenv
 load_dotenv()
 
 r = redis.Redis(host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'), password=os.getenv('DB_PASSWORD'),
-	decode_responses=True)
+decode_responses=True)
 s = requests.Session()
 
 def set_last_block():
-	next_block = last_known_block = int(r.get("last_included_block"))
-	while True:
-		if s.get(f"https://apis.matic.network/api/v1/matic/block-included/{next_block + 1}").json()["message"] != "success":
-			break
-		else:
-			next_block += 1
-	if next_block > last_known_block:
-		r.set("last_included_block", next_block)
+    while True:
+        next_block = last_known_block = int(r.get("last_included_block"))
+        while True:
+            if s.get(
+                f"https://apis.matic.network/api/v1/matic/block-included/{next_block + 1}"
+                ).json()["message"] != "success":
+                break
+            else:
+                next_block += 1
+            if next_block > last_known_block:
+                r.set("last_included_block", next_block)
+            time.sleep(3600)
 
 if __name__ == "__main__":
-	set_last_block()
+    set_last_block()
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
-import requests
+import os
 import redis
+import requests
+import threading
+import multiprocessing as mp
+import caching
 from fastapi import FastAPI
 from typing import Optional
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI()
-session = requests.Session()
+r = redis.Redis(host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'), password=os.getenv('DB_PASSWORD'),
+    decode_responses=True)
+s = requests.Session()
+
+cron = mp.Process(target=caching.set_last_block, daemon=True)
+cron.start()
 
 @app.get("/")
 async def read_root():
@@ -12,8 +23,12 @@ async def read_root():
 
 @app.get("/gas_overview")
 async def fetch_from_oracle():
-	return session.get("https://gasstation-mainnet.matic.network").json()
+    return s.get("https://gasstation-mainnet.matic.network").json()
 
 @app.get("/last_block")
 async def last_block():
-	pass
+    return r.get("last_included_block")
+
+@app.get("/debug")
+async def check():
+    return cron.pid
