e674d3ec2cdfb2458b6816479c14b5473b8f7f00
QEDK <1994constant@gmail.com>
2021-03-09 18:07:55 +0530

    :rocket: Optimize scraping to make it faster

--- backend/caching.py
+++ backend/caching.py
@@ -13,14 +13,10 @@ def set_last_block():
     while True:
         next_block = last_known_block = int(r.get("last_included_block"))
         while True:
-            if s.get(
-                f"https://apis.matic.network/api/v1/matic/block-included/{next_block + 1}"
-                ).json()["message"] != "success":
+            res = s.get(f"https://apis.matic.network/api/v1/matic/block-included/{next_block + 1}").json()
+            if res["message"] != "success":
                 break
-            next_block += 1
-            if next_block - last_known_block > 100: # update every 100 blocks anyway if lagging
-                last_known_block = next_block
-                r.set("last_included_block", next_block)
+            next_block = int(res["end"])
         if next_block > last_known_block:
             r.set("last_included_block", next_block)
         time.sleep(3600)
