9220791d37a02e49265e6b0cc22f8c5c2dd5a3eb
QEDK <1994constant@gmail.com>
2021-03-12 06:52:56 +0530

    :clock: Redesign scheduling

--- backend/caching.py
+++ backend/caching.py
@@ -2,24 +2,30 @@ import os
 import time
 import redis
 import requests
+import sched
 from dotenv import load_dotenv
 load_dotenv()
 
 r = redis.Redis(host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'), password=os.getenv('DB_PASSWORD'),
 decode_responses=True)
 s = requests.Session()
+schdler = sched.scheduler(time.time, time.sleep)
 
 def set_last_block():
+    next_block = last_known_block = int(r.get("last_included_block"))
     while True:
-        next_block = last_known_block = int(r.get("last_included_block"))
-        while True:
-            res = s.get(f"https://apis.matic.network/api/v1/matic/block-included/{next_block + 1}").json()
-            if res["message"] != "success":
-                break
-            next_block = int(res["end"])
-        if next_block > last_known_block:
-            r.set("last_included_block", next_block)
-        time.sleep(3600)
+        res = s.get(f"https://apis.matic.network/api/v1/matic/block-included/{next_block + 1}").json()
+        if res["message"] != "success":
+            break
+        next_block = int(res["end"])
+    if next_block > last_known_block:
+        r.set("last_included_block", next_block)
+
+def main():
+    set_last_block()
+    while True:
+        schdler.enter(3600, set_last_block)
+        schdler.run()
 
 if __name__ == "__main__":
-    set_last_block()
+    main()
