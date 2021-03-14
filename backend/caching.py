ea4a0255d4f8582988261d527f495ffde0087d27
QEDK <1994constant@gmail.com>
2021-03-14 15:03:21 +0530

    :new: Add caching for weekly prices

--- backend/caching.py
+++ backend/caching.py
@@ -3,6 +3,7 @@ import time
 import redis
 import requests
 import sched
+from datetime import date, timedelta
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -21,11 +22,25 @@ def set_last_block():
     if next_block > last_known_block:
         r.set("last_included_block", next_block)
 
+def set_weekly_prices():
+    to_date = date.today()
+    from_date = to_date - timedelta(weeks=1)
+    res = s.get(f"https://api.covalenthq.com/v1/pricing/historical/USD/MATIC/",
+        params={"from": from_date.isoformat(), "to": to_date.isoformat()}).json()
+    d = {}
+    for item in res["data"]["prices"]:
+        d[item["date"]] = item["price"]
+    r.hset("prices", mapping=d)
+
+def update_current_prices():
+    res = s.get(f"https://api.covalenthq.com/v1/pricing/historical/USD/MATIC")
+
 def main():
     set_last_block()
+    set_weekly_prices()
     while True:
         schdler.enter(3600, set_last_block)
         schdler.run()
 
 if __name__ == "__main__":
-    main()
+    update_current_prices()
